c0f4ccf0ddca834038f791ef1ebfda186cb9f8df
turkey2349 <Turkey2349@gmail.com>
2016-04-28 13:36:10 -0400

    - Updated camera and terrain code

--- src/main/java/VolatiliaOGL/game/Terrain.java
+++ src/main/java/VolatiliaOGL/game/Terrain.java
@@ -1,7 +1,5 @@
 package main.java.VolatiliaOGL.game;
 
-import java.awt.image.BufferedImage;
-
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -13,8 +11,6 @@ import main.java.VolatiliaOGL.util.MathUtil;
 public class Terrain
 {
 	public static final float SIZE = 800;
-	private static final float MAX_HEIGHT = 40;
-	private static final float MAX_PIXEL_COLOR = 256 * 256 * 256;
 
 	private float x;
 	private float z;
@@ -31,19 +27,6 @@ public class Terrain
 		this.x = gridX * SIZE;
 		this.z = gridZ * SIZE;
 		//this.model = this.generateTerrain(heightMap);
-	}
-
-	private float getHeight(int x, int y, BufferedImage image)
-	{
-		if(x < 0 || x >= image.getHeight() || y < 0 || y >= image.getHeight())
-			return 0;
-
-		float height = image.getRGB(x, y);
-
-		height += MAX_PIXEL_COLOR / 2f;
-		height /= MAX_PIXEL_COLOR / 2f;
-		height *= MAX_HEIGHT;
-		return height;
 	}
 
 	public float getHeightOfTerrain(float x, float z)
